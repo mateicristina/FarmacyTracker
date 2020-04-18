11b82a96225d45fdb10c1dac6a44e4b225e565db
Cristina Matei <cristina.matei0304@yahoo.com>
2020-04-18 19:44:49 +0300

    Initial Commit

---
+++ FarmacyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FarmacyTracker
+//
+//  Created by Cristina Matei on 18/04/2020.
+//  Copyright © 2020 Cristina-Gabriela Matei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
